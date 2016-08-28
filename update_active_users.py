eb58fb9cf80824a3dac03290dca2d94bdaf0edc5
suyooktang <suyook.tang@gmail.com>
2016-08-28 22:17:38 +0900

    활동적인 사용자 갱신하기

---
+++ update_active_users.py
@@ -0,0 +1,42 @@
+import mwclient as mw
+from collections import Counter
+from datetime import datetime, timedelta
+import os
+
+
+def main():
+    site = mw.Site('femiwiki.com')
+    site.login('훈장봇', os.environ['BOT_PW'])
+
+    now = datetime.now()
+    entries = fetch_recent_changes(
+        site,
+        now - timedelta(days=7),
+        now,
+    )
+    counts = Counter(sorted(entry['user'] for entry in entries)).most_common(20)
+    page = site.pages['페미위키:활동적인 사용자']
+    template = []
+    template.append('== 활동적인 사용자 ==')
+    for user, n in counts:
+        template.append('# [[사용자:%s|%s]]: <span class="count">%d</span>회' % (user, user, n))
+    page.save('\n'.join(template), '활동적인 사용자 갱신')
+
+
+def fetch_recent_changes(site, date_from, date_to):
+    result = site.api(
+        'query',
+        list='recentchanges',
+        rctype='edit|new',
+        rcshow='!bot|!anon',
+        rcprop='timestamp|user|title',
+        rclimit=5000,
+        rcdir='newer',
+        rcstart=date_from.strftime('%Y%m%d%H%M%S'),
+        rcend=date_to.strftime('%Y%m%d%H%M%S')
+    )
+    return result['query']['recentchanges']
+
+
+if __name__ == '__main__':
+    main()
