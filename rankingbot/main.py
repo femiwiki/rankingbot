db2f1a00dd3d5d32c29db87997c77e591eb04422
lens0021 <lorentz0021@gmail.com>
2019-06-26 01:15:23 +0900

    사용자 하이퍼링크를 기여 목록으로 변경
    Issue: https://femiwiki.com/w/%EC%A3%BC%EC%A0%9C:Uz6jrnp481sjvpii

--- rankingbot/main.py
+++ rankingbot/main.py
@@ -57,7 +57,7 @@ def main():
 
         template.append('|-')
         template.append(
-            '| %d || [[사용자:%s|%s]] ' % (i + 1, name, name))
+            '| %d || [[특수:기여/사용자:%s|%s]] ' % (i + 1, name, name))
 
     # Update the page
     wiki.save(
